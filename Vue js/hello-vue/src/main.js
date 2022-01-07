bc8158e8931998de0cf5ceb918e4f9883760491a
Zozylia <zozyliazozo22@gmail.com>
2022-01-07 20:37:15 +0200

    Exam2

--- Vue js/hello-vue/src/main.js
+++ Vue js/hello-vue/src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
 
+import { MdButton, MdContent, MdTabs, MdCard, MdRipple, MdMenu, MdList, MdField, MdSnackbar } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(VueCompositionAPI)
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdCard)
+Vue.use(MdRipple)
+Vue.use(MdMenu)
+Vue.use(MdList)
+Vue.use(MdField)
+Vue.use(MdSnackbar)
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- Vue js/hello-vue/src/views/base/Base.vue
+++ Vue js/hello-vue/src/views/base/Base.vue
@@ -1,33 +1,30 @@
 <template>
-    <section id="app2">
-        <h1>Base section {{name}}</h1>
-        <base1></base1>
-        <button v-on:click="upNumber()">{{numberUse}}</button>
-    </section>
+  <section id="app2">
+    <h1>Base section {{ name }}</h1>
+    <base1></base1>
+    <button v-on:click="upNumber()">{{ numberUse }}</button>
+  </section>
 </template>
 <script>
-import Base1 from "./Base1.vue"
+import Base1 from "./Base1.vue";
 export default {
-    name: "base",
-    components: {
-        Base1
+  name: "base",
+  components: {
+    Base1,
+  },
+  el: "#app2",
+  data: () => ({
+    name: "QWERTY",
+    version: "2.34.12.3",
+    numberUse: 5,
+  }),
+  computed: {},
+  methods: {
+    upNumber: function () {
+      return this.numberUse++;
     },
-    el: "#app2",
-    data: () => ({
-        
-        name: "QWERTY",
-        version: "2.34.12.3",
-        numberUse: 5,
-        
-    }),
-    computed: {},
-    methods: {
-    upNumber:  function () {
-       return this.numberUse++;
-      }
-        }
-}
+  },
+};
 </script>
 <style scoped>
-
 </style>
